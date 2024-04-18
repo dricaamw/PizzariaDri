d1c812f6cd8d2ce8498254a7a506db88467018bb
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-04-18 11:56:40 -0300

    initial

---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageLayout from "../layouts/PageLayout";
+import Home from "../pages/sitePages/Home";
+
+const Paths = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<PageLayout />}>
+                        <Route index element={<Home />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>            
+        </>
+    );
+}
+
+export default Paths;
